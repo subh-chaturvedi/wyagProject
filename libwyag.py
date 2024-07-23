b1291adafd10259805d333775e8acdfecd3c0a0a
subhchaturvedi <subh.chaturvedi2021@vitbhopal.ac.in>
2024-07-24 00:29:19 +0530

    commit

--- libwyag.py
+++ libwyag.py
@@ -319,3 +319,26 @@ argsp.add_argument("path",
                    help="Read object from <file>")
 
 
+def cmd_hash_object(args):
+    if args.write:
+        repo = repo_find()
+    else:
+        repo = None
+
+    with open(args.path, "rb") as fd:
+        sha = object_hash(fd, args.type.encode(), repo)
+        print(sha)
+        
+def object_hash(fd, fmt, repo=None):
+    """ Hash object, writing it to repo if provided."""
+    data = fd.read()
+
+    # Choose constructor according to fmt argument
+    match fmt:
+        case b'commit' : obj=GitCommit(data)
+        case b'tree'   : obj=GitTree(data)
+        case b'tag'    : obj=GitTag(data)
+        case b'blob'   : obj=GitBlob(data)
+        case _: raise Exception("Unknown type %s!" % fmt)
+
+    return object_write(obj, repo)
